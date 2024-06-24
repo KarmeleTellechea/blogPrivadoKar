881c785b4d2175b88467d40e3e1ef4fe2c5ad684
Karmele <ktelletxea.kt@gmail.com>
2024-06-24 13:00:54 +0200

    no furrula el pop

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Funcion Boton 
 function boton(){
 const condiciones  = document.getElementById("checkcondiciones");
 const enviar = document.getElementById("btnenviar");
@@ -6,7 +7,18 @@ const cambiarCondiciones = () => { enviar.disabled = !condiciones.checked;};
 //mandame a otra pagina
 condiciones.addEventListener('change', cambiarCondiciones);
 }
-
+// Funcion POPUP
+    //funcion aparecer 
+    function Sipop(title, text) {
+       const title= document.getElementById("poptitulo").innerHTML = title;
+       const text= document.getElementById("poptexto").innerHTML = text;
+        document.getElementById("t_c_pop").classList.add('show');
+    }
+    Sipop()
+     //funcion desaparecer 
+    function Nopop() {
+        document.getElementById("t_c_pop").classList.remove('show');
+    }
 
 
 
