69ae237bf39c1bc97a1d94097fa0c0cb7a1609e5
Karmele <ktelletxea.kt@gmail.com>
2024-06-24 12:13:12 +0200

    bnt desactivado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 function boton(){
-const condiciones  = document.getElementById("condiciones");
-const enviar = document.getElementById("enviar");
+const condiciones  = document.getElementById("checkcondiciones");
+const enviar = document.getElementById("btnenviar");
 //condiciones ?
 const cambiarCondiciones = () => { enviar.disabled = !condiciones.checked;};
 //mandame a otra pagina
 condiciones.addEventListener('change', cambiarCondiciones);
 }
+
+
+
+
+
+
+
+
+boton()
