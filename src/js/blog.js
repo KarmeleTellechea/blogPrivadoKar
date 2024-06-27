2cc390bb20eea20669dff45cc288f2f1cc3a62e2
Karmele <ktelletxea.kt@gmail.com>
2024-06-27 12:20:07 +0200

    web component

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,56 +1,46 @@
-//logueate primero
-const urlauth = new URLSearchParams(window.location.search);
-const authen = urlauth.get('auth');
-if (!authen) {
-    alert('Registrate');
-    window.location = 'loging.html';}
-// articulos blog 
+import { miComponenteWeb } from './componente.js';
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const contenedorArticulos = document.getElementById('contenedorArticulos');
-        const lista = document.getElementById('lista');
+window.customElements.define('custom-tag', miComponenteWeb);
 
-        cargarArticulos();//para ver si funciona
-    
-        //  cargar todos los artículos
-        function cargarArticulos() {
-            fetch('../articles.json') 
-                .then(response => response.json())
-                .then(data => {
-                    renderizarArticulos(data.natura);//natura por que es lo que tengo
-                    agregarEventosFiltros(data); 
-                })
-                .catch(error => console.error('Error al cargar el JSON:', error));
-        }
-    
-        // renderizar los artículos 
-        function renderizarArticulos(articulos) {
-            contenedorArticulos.innerHTML = ''; 
-    
-            articulos.forEach(articulo => {
-                const articleDiv = document.createElement('div');
-                articleDiv.classList.add('article-card');
-    
-                articleDiv.innerHTML = `
-                    <img src="${articulo.image}" alt="${articulo.title}">
-                `;
-    
-                contenedorArticulos.appendChild(articleDiv);
-            });
-        }
-    
-        // Función para agregar eventos a los filtros
-        function agregarEventosFiltros(data) {
-            lista.addEventListener('click', function (event) {
-                event.preventDefault(); 
-                if (event.target.tagName === 'A') {
-                    const categoria = event.target.dataset.category;
-                    console.log("categoria")
-                    const articulosFiltrados = data[categoria];
-    
-                    renderizarArticulos(articulosFiltrados);
-                }
-            });
-        }
-    });
-    
+
+document.addEventListener('DOMContentLoaded', function () {
+    const contenedorArticulos = document.getElementById('contenedorArticulos');
+    const lista = document.getElementById('lista');
+
+    cargarArticulos();
+
+    function cargarArticulos() {
+        fetch('../articles.json')
+            .then(response => response.json())
+            .then(data => {
+                renderizarArticulos(data.natura);
+                agregarEventosFiltros(data);
+            })
+            .catch(error => console.error('Error al cargar el JSON:', error));
+    }
+
+    function renderizarArticulos(articulos) {
+        contenedorArticulos.innerHTML = '';
+
+        articulos.forEach(articulo => {
+            const articleElement = document.createElement('custom-tag');
+            
+            articleElement.setAttribute('image-src', articulo.image);
+           
+            contenedorArticulos.appendChild(articleElement);
+        });
+    }
+
+    function agregarEventosFiltros(data) {
+        lista.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (event.target.tagName === 'A') {
+                const categoria = event.target.dataset.category;
+                const articulosFiltrados = data[categoria];
+
+                renderizarArticulos(articulosFiltrados);
+            }
+        });
+    }
+});
+
---
+++ src/js/componente.js
@@ -0,0 +1,64 @@
+export class miComponenteWeb extends HTMLElement {
+    constructor() {
+        super();
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <div class="article-card">
+                <img class="article-image" src="" alt="">
+            </div>
+            <style>
+          
+                .article-card {
+                    width: calc( 20rem);
+                    box-sizing: border-box;
+                    text-align: center;
+                    background-color: #f9f9f9;
+                    transition: background-color 0.3s ease;
+                    border-radius: 1rem;
+                    padding: 1rem;
+                    margin: 2rem;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+
+                .article-image {
+                    width: 100%;
+                    height: auto;
+                    max-height: 12rem;
+                    object-fit: cover;
+                    border-radius: 1rem;
+                }
+
+                .article-card:hover {
+                    background-color: #c5c4c4;
+                }
+
+                @media (max-width: 1200px) {
+                    .article-card {
+                        width: calc(10rem);
+                    }
+                }
+
+                @media (max-width: 768px) {
+                    .article-card {
+                        width: calc(100% - 1rem);
+                    }
+                }
+            </style>
+        `;
+
+        const root = this.attachShadow({ mode: "open" });
+        root.appendChild(document.importNode(template.content, true))
+    }
+
+    connectedCallback() {
+        const imageSrc = this.getAttribute('image-src');
+        const title = this.getAttribute('title');
+        const shadow = this.shadowRoot;
+        shadow.querySelector('.article-image').src = imageSrc;
+        shadow.querySelector('.article-image').alt = title;
+    }
+}
+
