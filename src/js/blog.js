fd5ed154765d920239664514f2c38196dd84a069
Karmele <ktelletxea.kt@gmail.com>
2024-06-27 12:48:07 +0200

    llevar mas info al url para poder identificar

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,17 +1,17 @@
 import { miComponenteWeb } from './componente.js';
 
 window.customElements.define('custom-tag', miComponenteWeb);
-//logueate primer
+//-----------------------------------------------------------logueate primer
 const urlauth = new URLSearchParams(window.location.search);
 const authen = urlauth.get('auth');
 if (!authen) {
     alert('Registrate');
     window.location = 'loging.html';}
-//pilla el contenedor y las listas
+//-----------------------------------------------------------pilla el contenedor y las listas
 document.addEventListener('DOMContentLoaded', function () {
     const contenedorArticulos = document.getElementById('contenedorArticulos');
     const lista = document.getElementById('lista');
-//al iniciar empieza con natura 
+//----------------------------------------------------------al iniciar empieza con natura 
     cargarArticulos();
     function cargarArticulos() {
         fetch('../articles.json')
@@ -21,23 +21,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 agregarEventosFiltros(data);
             })    
     }
-//renderiza los web compnentes arriba importados 
+//----------------------------------------------------------renderiza los web compnentes arriba importados 
     function renderizarArticulos(articulos) {
-        contenedorArticulos.innerHTML = '';//refresca
-        //por cada articulo creame un custom-tag
+        contenedorArticulos.innerHTML = '';//-----------------refresca
+        //----------------------------------------------------por cada articulo creame un custom-tag
         articulos.forEach(articulo => {
             const articleElement = document.createElement('custom-tag');
             
             articleElement.setAttribute('image-src', articulo.image);
-            //levame a otro html
+            //---------------------------------------------------levame a otro html
             articleElement.addEventListener('click', function () {
-                window.location.href = 'articulos.html';
-                //seguramente tendre que llevame el tag o id para identificar 
+                window.location.href =  `articulos.html?id=${articulo.labels}`;
+              
+                //------------------------------------------------seguramente tendre que llevame el tag o id para identificar 
             });
             contenedorArticulos.appendChild(articleElement);
         });
     }
-//filtralos por datos
+//--------------------------------------------------------------------filtralos por datos
     function agregarEventosFiltros(data) {
         lista.addEventListener('click', function (event) {
             event.preventDefault();
